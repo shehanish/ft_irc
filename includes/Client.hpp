d72da4900f19462195085c475b45d67e373b3078
Sabina Pitul <sabina_pitul@yahoo.de>
2025-09-22 13:03:58 +0200

    client.hpp

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -3,17 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   Client.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lde-taey <lde-taey@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: spitul <spitul@student.42berlin.de >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 16:56:28 by lde-taey          #+#    #+#             */
-/*   Updated: 2025/08/12 17:00:05 by lde-taey         ###   ########.fr       */
+/*   Updated: 2025/09/22 12:47:54 by spitul           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
 
 class Client
 {
 	public:
-
+		int	getFd() { return _fd; }
+		std::set<Channel *>	getUserChannel()	{ return _channels; }
+		bool	hasChannel(Channel *channel)	{ return (_channels.find(channel) != _channels.end()); }
+		void	addUserChannel(Channel *channel)	
+		{
+			if (!hasChannel(channel))
+				_channels.insert(channel); 
+		}
+		void	delUserChannel(Channel *channel)	
+		{
+			if (hasChannel(channel))
+				_channels.erase(channel); 
+		}
+		int	getNbChannel()	{ return _nb_chan; }
+		std::string	getName()	{ return _nick; }
+		void	addChannel()	{ _nb_chan++; }
+		void	sendMsg(Client &client, std::string &msg)	
+		{ 
+			if (!msg.empty())
+				send(client._fd, msg, msg.length(), 0);
+		}
+			
 	private:
+		int	_fd;
+		int	_nb_chan;
+		std::string	_nick;
+		std::string	_buffer;
+		std::set<Channel *>	_channels;
 	
 };
+
+const int MAX_CHANNELS = 10;
+
+#endif
